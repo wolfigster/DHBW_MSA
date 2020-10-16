707c690e1d870474ef215a82a4994b85149ded7e
Johannes <Jeis2519@gmail.com>
2020-10-17 00:44:08 +0200

    add simulation method to reset the hsqldb and fill it with data

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,27 +1,75 @@
 package application;
 
+import logger.AlgorithmType;
+import msa.Channel;
+import msa.Participant;
+import msa.ParticipantType;
 import persistence.HSQLDB;
+import persistence.tables.*;
 
 public class Application {
     public static void main(String... args) {
-        // hsqldb demo
+        simulateHSQLDB();
+    }
+
+
+    private static void simulateHSQLDB() {
         HSQLDB.instance.setupConnection();
 
-        HSQLDB.instance.dropTableParticipants();
-        HSQLDB.instance.dropTableTypes();
+        // drop all tables
+        for(Participant participant : ParticipantTable.getParticipants()) PostboxTable.dropTable(participant.getName());
+        AlgorithmTable.dropTable();
+        ChannelTable.dropTable();
+        MessageTable.dropTable();
+        ParticipantTable.dropTable();
+        TypeTable.dropTable();
 
-        HSQLDB.instance.createTableTypes();
-        HSQLDB.instance.createTableParticipants();
+        // create all tables
+        AlgorithmTable.createTable();
+        TypeTable.createTable();
+        ParticipantTable.createTable();
+        ChannelTable.createTable();
+        MessageTable.createTable();
 
-        HSQLDB.instance.insertDataTableTypes("normal");
-        HSQLDB.instance.insertDataTableTypes("intruder");
+        // insert data in tables for simulation purposes
+        // insert Algorithms
+        for(AlgorithmType algorithmType : AlgorithmType.values()) {
+            if(algorithmType != AlgorithmType.NONE) {
+                AlgorithmTable.insertAlgorithm(algorithmType.getName());
+            }
+        }
 
-        HSQLDB.instance.insertDataTableParticipants("branch_hkg", 1);
-        HSQLDB.instance.insertDataTableParticipants("branch_cpt", 1);
-        HSQLDB.instance.insertDataTableParticipants("branch_sfo", 1);
-        HSQLDB.instance.insertDataTableParticipants("branch_syd", 1);
-        HSQLDB.instance.insertDataTableParticipants("branch_wuh", 1);
-        HSQLDB.instance.insertDataTableParticipants("msa", 2);
+        // insert Types
+        for(ParticipantType participantType : ParticipantType.values()) TypeTable.insertType(participantType.getType());
+
+        // insert Participants
+        Participant branch_hkg = new Participant(1, "branch_hkg", ParticipantType.NORMAL);
+        Participant branch_cpt = new Participant(2, "branch_cpt", ParticipantType.NORMAL);
+        Participant branch_sfo = new Participant(3, "branch_sfo", ParticipantType.NORMAL);
+        Participant branch_syd = new Participant(4, "branch_syd", ParticipantType.NORMAL);
+        Participant branch_wuh = new Participant(5, "branch_wuh", ParticipantType.NORMAL);
+        Participant msa = new Participant(6, "msa", ParticipantType.INTRUDER);
+
+        ParticipantTable.insertParticipant(branch_hkg);
+        ParticipantTable.insertParticipant(branch_cpt);
+        ParticipantTable.insertParticipant(branch_sfo);
+        ParticipantTable.insertParticipant(branch_syd);
+        ParticipantTable.insertParticipant(branch_wuh);
+        ParticipantTable.insertParticipant(msa);
+
+        // add Postboxes
+        for(Participant participant : ParticipantTable.getParticipants()) PostboxTable.createTable(participant.getName());
+
+        // insert Channels
+        Channel hkg_wuh = new Channel("hkg_wuh", branch_hkg, branch_wuh);
+        Channel hkg_cpt = new Channel("hkg_cpt", branch_hkg, branch_cpt);
+        Channel cpt_syd = new Channel("cpt_syd", branch_cpt, branch_syd);
+        Channel syd_sfo = new Channel("syd_sfo", branch_syd, branch_sfo);
+
+        ChannelTable.insertChannel(hkg_wuh);
+        ChannelTable.insertChannel(hkg_cpt);
+        ChannelTable.insertChannel(cpt_syd);
+        ChannelTable.insertChannel(syd_sfo);
 
         HSQLDB.instance.shutdown();
     }
