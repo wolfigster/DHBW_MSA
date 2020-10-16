dae06b928264086054648098c7d38a17522b09a5
Johannes <Jeis2519@gmail.com>
2020-10-17 00:39:20 +0200

    add IntrudeChannelCmd functionality

--- src/command/IntrudeChannelCmd.java
+++ src/command/IntrudeChannelCmd.java
@@ -2,26 +2,46 @@ package command;
 
 import msa.Channel;
 import msa.Participant;
+import msa.ParticipantType;
 import persistence.tables.ChannelTable;
 import persistence.tables.ParticipantTable;
 
 public class IntrudeChannelCmd implements ICommand {
 
-    private final Channel channel;
-    private final Participant participant;
+    private final String channelName;
+    private final String participantName;
 
     public IntrudeChannelCmd(String channelName, String participantName) {
         System.out.println("IntrudeChannelCmd was constructed");
 
-        this.channel = ChannelTable.getChannel(channelName);
-        this.participant = ParticipantTable.getParticipantByName(participantName);
-
+        this.channelName = channelName;
+        this.participantName = participantName;
     }
 
     @Override
     public String execute() {
         System.out.println("Run command IntrudeChannelCmd");
         StringBuilder response = new StringBuilder();
+
+        Channel channel = ChannelTable.getChannel(channelName);
+        if(channel == null) {
+            response.append("unknown channel ").append(channelName);
+            return response.toString();
+        }
+
+        Participant participant = ParticipantTable.getParticipantByName(participantName);
+        if(participant == null) {
+            response.append(participantName).append(" doesn't exist");
+            return response.toString();
+        }
+
+        if(!participant.getType().equals(ParticipantType.INTRUDER)) {
+            response.append(participantName).append(" isn't an intruder");
+            return response.toString();
+        }
+
+        channel.setIntruder(participant);
+        response.append(participant.getName()).append(" intrudes now channel ").append(channel.getName());
 
         return response.toString();
     }
